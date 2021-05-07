6662a08096f15ccfedfd196bf0ae1c064c275602
Bognaum <bognaum@gmail.com>
2021-05-07 11:42:35 +0300

    Deleted unuseful

--- Ace_PlugKit/get_file_code.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function get_file_code($pathname, $encode_from="", $encode_to="")
-{
-	$code = file_get_contents($pathname);
-	if ($encode_from && $encode_to)
-		$code = iconv($encode_from, $encode_to, $code);
-	return $code;
-}
